8b29d7e34e6d9959395764dbb89adf10c05f9be7
Matias Laudadio <matlaudadio@gmail.com>
2019-02-21 20:32:49 -0300

    Maquetacion de LOGIN PAGE y REGISTER PAGE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import {LoginPageComponent} from './componentes/login-page/login-page.component'
 import {RegisterPageComponent} from './componentes/register-page/register-page.component';
 import {PrivadoComponent} from './componentes/privado/privado.component';
 import {NotFaundPageComponent} from './componentes/not-faund-page/not-faund-page.component';
-import { from } from 'rxjs';
+
 const routes: Routes = [
   {path:'', component: HomePageComponent},
   {path:'login', component:LoginPageComponent},
