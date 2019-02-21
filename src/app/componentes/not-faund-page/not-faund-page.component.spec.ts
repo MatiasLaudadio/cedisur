8e5b572d49f28ddfb6fafd373494eba5f6a44880
Matias Laudadio <matlaudadio@gmail.com>
2019-02-21 13:40:19 -0300

    Creacion de componentes

---
+++ src/app/componentes/not-faund-page/not-faund-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NotFaundPageComponent } from './not-faund-page.component';
+
+describe('NotFaundPageComponent', () => {
+  let component: NotFaundPageComponent;
+  let fixture: ComponentFixture<NotFaundPageComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NotFaundPageComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NotFaundPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
